8b25b434737c84de3a4c5a2ebd0a327339657a3b
Ant Lion <johndoe@ms.com>
2022-04-26 20:27:25 +0300

    home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div style="background-color:black" class="text-white">
+    <div class="flex justify-between">
+        <img src="/images/logo.png" alt="" style="width: 75px;" class="m-16">
+        <div>
+            <button class="border-yellow-500 border-2 rounded-lg border-white m-16 p-4 text-2xl"> Connect Wallet </button>
+        </div>  
+    </div>
+
+    <br>
+
+    <div class="flex justify-center">
+        <img class="mr-20" src="images/3man1.png" alt="" style="width: 150px;">
+        <div>
+        <div class="border-yellow-500 border-2 rounded-lg border-white p-6 text-3xl">
+            <p>Initially thr33p3nny is available in rolls of 33.3</p>
+            <p>100 Rolls. 33.3 XTZ each</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="text-5xl flex justify-center m-20 ">
+        <div class="border-yellow-500 border-2 rounded-lg border-white p-6">
+        <p>thr33p3nnythe Thr33som3s utility token</p>
+        <p>ticker: 3P</p>
+        <p>Contract Address: tzkt link</p>
+        </div>
+    </div>
+   
+    <div class="flex justify-center">
+        <progress class="w-2/4 mb-20" value="32" max="100"> Sold Out</progress>
+    </div>
+   
+    <div class="flex justify-center space-x-9 ">
+        <button class="border-yellow-500 border-2 rounded-lg border-white py-3 px-6 text-2xl">Buy 1 Rolls</button>
+        <button class="border-yellow-500 border-2 rounded-lg border-white py-3 px-6 text-2xl">Buy 2 Rolls</button>
+    </div>
+   
+    <div class="flex justify-center mt-20">
+        <img src="" alt="spinning coin">
+    </div>
+   
+    <div class="flex justify-center mt-16 space-x-9 ">
+        <a href="https://discord.gg/FppsEtzUr4">
+        <img src="images/discord.png" alt="" style="width: 95px;height:70px;">
+      </a>
+        <a href="https://twitter.com/houseoftez">
+        <img src="images/twitter.png" alt="" style="width: 70px;height:70px;">
+      </a>
+    </div>
+
+    <div class="flex flex-wrap justify-center mt-6 mb-2" text="#00ff00">
+       ©️2022 Made by Boris Grit at TezHouse In collaboration with Sebuh Squad for the Grotto
+    </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
